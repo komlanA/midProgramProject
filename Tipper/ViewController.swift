86c7aa9d5f1ea86a4eea0f51d2be8a04fd18111c
Komlan Attiogbe <jonathan.attiogbe@gmail.com>
2019-01-22 16:28:32 -0500

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Komlan Attiogbe on 1/22/19.
+//  Copyright © 2019 Komlan Attiogbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
